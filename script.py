b58d52a5ec74c43be6d7ed0acdb61e7de94cbfe9
Aayushsingh2001 <as6994770@gmail.com>
2024-12-11 12:57:36 +0530

    Added codes

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/ping')
 def ping():
-    return 'pong'
+    return 'Niki (I LOVE YOU SOO MUCH) Darling....'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=2001)
